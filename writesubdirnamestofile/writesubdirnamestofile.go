7e92b7f0849b4a91e5a1ddb2225a6b064d9d4875
LuisGSandoval <luis.sandoval.457@outlook.com>
2020-09-23 20:23:26 -0500

    finished file directory exercises

--- writesubdirnamestofile/writesubdirnamestofile.go
+++ writesubdirnamestofile/writesubdirnamestofile.go
@@ -3,8 +3,8 @@ package writesubdirnamestofile
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
-	"path/filepath"
 )
 
 var (
@@ -17,90 +17,47 @@ var (
 // WriteSubDirNamesToFile testing
 func WriteSubDirNamesToFile() {
 
-	if len(os.Args) < 2 {
-		fmt.Println("Please add 1 directory path")
-		return
+	paths := os.Args[1:]
+	if len(paths) < 1 {
+		log.Panic("not enough folder paths to process, \nplease add a folder to read the diretories name")
+
 	}
 
-	args := os.Args[1:]
+	var dirs []byte
 
-	fileRoute := args[0]
+	for _, dir := range paths {
 
-	err := os.Remove(fullWorkPath) // this removes the file where the info is going to be written on
-
-	if err != nil {
-		fmt.Println("[Error] WriteSubDirNamesToFile():", err.Error())
-		return
-	}
-
-	writeDirNames(fileRoute)
-
-}
-
-// This is the main function of the app
-func writeDirNames(fileRoute string) {
-
-	checkFileExistance()
-
-	for _, data := range readDirInfo(fileRoute) {
-
-		fileContent, err := ioutil.ReadFile(fullWorkPath)
+		file, err := ioutil.ReadDir(dir)
 
 		if err != nil {
-			fmt.Println("[Error] writeDirNames: ", err.Error())
-			return
+			log.Panic("Error: ", err.Error())
 		}
 
-		if data.IsDir() {
-			fullFilePath, err := filepath.Abs(data.Name())
-			if err != nil {
-				fmt.Println("[Error], writeDirNames: ", err.Error())
+		dirs = append(dirs, dir+"\n"...)
+
+		for _, fileData := range file {
+			if fileData.IsDir() {
+
+				dirs = append(dirs, "\t"+fileData.Name()+"\n"...)
+
+				secondPath := dir + fileData.Name() + "/"
+
+				file2, err := ioutil.ReadDir(secondPath)
+
+				if err != nil {
+					log.Fatal("Error:", err.Error())
+				}
+
+				for _, fileData2 := range file2 {
+					dirs = append(dirs, "\t \t"+fileData2.Name()+"\n"...)
+				}
+
 			}
-
-			dueChecking = append(dueChecking, fullFilePath)
-			fileContent = append(fileContent, data.Name()+"/ \n"...)
-			ioutil.WriteFile(fullWorkPath, fileContent, 0644)
 		}
 
-		for _, filePathToCkeck := range dueChecking {
-			writeDirNames(filePathToCkeck)
-		}
 	}
 
-}
+	ioutil.WriteFile(fullWorkPath, dirs, 0644)
+	fmt.Printf("\n%s \n", dirs)
 
-// this file ensures that the file where we're going to write, acually exists if not, it creates the file
-func checkFileExistance() {
-
-	var existance bool
-
-	fileInfo, err := ioutil.ReadDir(workingDir)
-
-	if err != nil {
-		fmt.Println("[Error] checkFileExistance:", err.Error())
-		return
-	}
-
-	for _, file := range fileInfo {
-		if file.Name() == outputFile {
-			existance = true
-			break
-		}
-	}
-
-	if !existance {
-		ioutil.WriteFile(fullWorkPath, []byte{}, 0644)
-	}
-}
-
-func readDirInfo(fileRoute string) []os.FileInfo {
-
-	fileInfo, err := ioutil.ReadDir(fileRoute)
-
-	if err != nil {
-		fmt.Println("[Error] readDirInfo: ", err.Error())
-		return nil
-	}
-
-	return fileInfo
 }
